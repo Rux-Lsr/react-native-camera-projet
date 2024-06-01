583b1f8faa790943403eda8f21dbbfede0616034
Your Name <you@example.com>
2024-06-01 06:01:30 +0100

    Sauvegarde dans la galerie

--- cameraApp.tsx
+++ cameraApp.tsx
@@ -1,7 +1,9 @@
 import React, { PureComponent } from 'react';
-import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Alert, Image, StyleSheet, Text, TouchableOpacity, View,PermissionsAndroid, Platform, ToastAndroid  } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import RNFS from 'react-native-fs';
+import { CameraRoll } from "@react-native-camera-roll/camera-roll";
+
 const icons ={
   flash :  require('./flash.png'),
   camera_switch:  require('./camera_switch.png')
@@ -22,21 +24,76 @@ const icons ={
   </View>
 );
 
-/**
- * Affiche une boîte de dialogue avec un titre et un message
- * @param {string} title - Le titre de la boîte de dialogue
- * @param {string} msg - Le message de la boîte de dialogue
- */
-const DialogMessage = (title : string, msg : string) => {
-  Alert.alert(
-    title,
-    msg,
-    [
-      { text: "OK", onPress: () => console.log("OK Pressed") }
-    ]
-  );
+
+
+async function hasAndroidPermission() {
+  const getCheckPermissionPromise = () => {
+    if (Platform.Version >= 33) {
+      return Promise.all([
+        PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES),
+        PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO),
+        PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE), // Ajout de la permission d'écriture
+      ]).then(
+        ([hasReadMediaImagesPermission, hasReadMediaVideoPermission, hasWriteExternalStoragePermission]) =>
+          hasReadMediaImagesPermission && hasReadMediaVideoPermission && hasWriteExternalStoragePermission,
+      );
+    } else {
+      return Promise.all([
+        PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE),
+        PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE), // Ajout de la permission d'écriture
+      ]).then(
+        ([hasReadExternalStoragePermission, hasWriteExternalStoragePermission]) =>
+          hasReadExternalStoragePermission && hasWriteExternalStoragePermission,
+      );
+    }
+  };
+
+  const hasPermission = await getCheckPermissionPromise();
+  if (hasPermission) {
+    return true;
+  }
+
+  const getRequestPermissionPromise = () => {
+    if (Platform.Version >= 33) {
+      return PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+        PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE, // Ajout de la permission d'écriture
+      ]).then(
+        (statuses) =>
+          statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+          statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+          statuses[PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE] ===
+            PermissionsAndroid.RESULTS.GRANTED,
+      );
+    } else {
+      return PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE, // Ajout de la permission d'écriture
+      ]).then(
+        (statuses) =>
+          statuses[PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+          statuses[PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE] ===
+            PermissionsAndroid.RESULTS.GRANTED,
+      );
+    }
+  };
+
+  return await getRequestPermissionPromise();
 }
 
+
+async function savePicture(tag:string, type, album:string) {
+  if (Platform.OS === "android" && !(await hasAndroidPermission())) {
+    return;
+  }
+
+  CameraRoll.saveAsset(tag, {type, album})
+  console.log("blalalalal")
+};
 
 export class CameraVision extends PureComponent {
   state = {
@@ -65,6 +122,55 @@ export class CameraVision extends PureComponent {
         : RNCamera.Constants.Type.back,
     });
   };
+
+    /**
+   * Commence l'enregistrement d'une vidéo
+   * @param {object} camera - L'objet caméra de RNCamera
+   */
+    startRecording = async (camera) => {
+      const timestamp = Date.now();
+      try {
+        this.setState({ isRecording: true });
+        const options = { quality: RNCamera.Constants.VideoQuality['480p'], path: `${RNFS.ExternalDirectoryPath}/video_${timestamp}.mp4` };
+        const data = await camera.recordAsync(options);
+        savePicture(data.uri, 'video', 'react-native-video')
+        console.log(`Video saved at: ${data.uri}`);
+        ToastAndroid.show(`Video saved at: ${data.uri})}`, ToastAndroid.LONG);
+        
+      } catch (error) {
+        console.error("Erreur lors de l'enregistrement :", error);
+      }
+    };
+    
+     /**
+     * Arrête l'enregistrement d'une vidéo
+     * @param {object} camera - L'objet caméra de RNCamera
+     */
+    stopRecording = (camera) => {
+      if (this.state.isRecording) {
+        camera.stopRecording();
+        this.setState({ isRecording: false });
+      }
+    };
+    
+    /**
+     * Prend une photo
+     * @param {object} camera - L'objet caméra de RNCamera
+     */
+    takePicture = async function (camera) {
+      const timestamp = Date.now();
+      this.state.isRecording =false
+      const options = { 
+        quality: 0.5, 
+        base64: true, path: 
+        `${RNFS.ExternalDirectoryPath}/photo_${timestamp}.jpg` 
+      };
+      
+      const data = await camera.takePictureAsync(options);
+      ToastAndroid.show(`Photo saved at: ${data.uri}`, ToastAndroid.LONG)
+      savePicture(options.path, 'photo', 'react-native-photo')
+      console.log(`Photo saved at: ${data.uri}`);
+    };
 
   render() {
     return (
@@ -124,66 +230,11 @@ export class CameraVision extends PureComponent {
     );
   }
   
-  /**
-   * Commence l'enregistrement d'une vidéo
-   * @param {object} camera - L'objet caméra de RNCamera
-   */
-  startRecording = async (camera) => {
-    const timestamp = Date.now();
-    try {
-      this.setState({ isRecording: true });
-      const options = { quality: RNCamera.Constants.VideoQuality['480p'], path: `${RNFS.ExternalDirectoryPath}/video_${timestamp}.mp4` };
-      const data = await camera.recordAsync(options);
-      console.log(`Video saved at: ${data.uri}`);
-      setTimeout(()=>{DialogMessage('Video', `Video saved at: ${data.uri}`)}, 1000)
-      refreshStorage(options.path)
 
-    } catch (error) {
-      console.error("Erreur lors de l'enregistrement :", error);
-    }
-  };
-  
-   /**
-   * Arrête l'enregistrement d'une vidéo
-   * @param {object} camera - L'objet caméra de RNCamera
-   */
-  stopRecording = (camera) => {
-    if (this.state.isRecording) {
-      camera.stopRecording();
-      this.setState({ isRecording: false });
-    }
-  };
-  
-  /**
-   * Prend une photo
-   * @param {object} camera - L'objet caméra de RNCamera
-   */
-  takePicture = async function (camera) {
-    const timestamp = Date.now();
-    this.state.isRecording =false
-    const options = { 
-      quality: 0.5, 
-      base64: true, path: 
-      `${RNFS.ExternalDirectoryPath}/photo_${timestamp}.jpg` 
-    };
-    
-    const data = await camera.takePictureAsync(options);
-    setTimeout(()=>DialogMessage("Info", "picture save to : "+options.path), 1000)
-    console.log(`Photo saved at: ${data.uri}`);
-    refreshStorage(options.path)
-  };
 
 }
 
-function refreshStorage(uri:string): void{
-  RNFS.scanFile(uri)
-  .then(() => {
-    console.log('Scanned photo file');
-  })
-  .catch((err) => {
-    console.log('Cannot scan photo file', err);
-  });
-}
+
  const styles = StyleSheet.create({
   container: {
     flex: 1,
